ad86411cfca29b2d579d4fef6bc1b529f01b2b4b
RizkiyanaPutri <rizkiyanaputri8e30@gmail.com>
2025-10-13 21:41:24 +0700

    revisi kode program pada struktur kontrol dan output

--- CaseMethod_1.java
+++ CaseMethod_1.java
@@ -5,12 +5,13 @@ public class CaseMethod_1 {
         double uts=0.3, uas=0.4, tugas=0.3;
         String nilaiHuruf_1, nilaiHuruf_2, status_1, status_2, statusAkhir;
 
+        System.out.println("<<<<<INPUT DATA MAHASISWA>>>>");
         System.out.print("Nama Mahasiswa: >");
         String namaMhs = sc.nextLine();
-        System.out.print("Masukkan NIM: >");
+        System.out.print("Masukkan NIM  : >");
         String nim = sc.nextLine();
 
-        System.out.println("Mata Kuliah 1: Algoritma dan Pemrograman");
+        System.out.println("<<Algoritma dan Pemrograman>>");
         System.out.print("Input nilai UTS: >");
         int nilaiUTS_1= sc.nextInt();
         System.out.print("Input nilai UAS: >");
@@ -19,7 +20,7 @@ public class CaseMethod_1 {
         int nilaiTugas_1 = sc.nextInt();
         double nilaiAkhir_1 = (nilaiUTS_1 * uts) + (nilaiUAS_1 * uas) + (nilaiTugas_1 * tugas);
 
-        System.out.println("Mata Kuliah 2: Struktur Data");
+        System.out.println("<<<<<<<<Struktur Data>>>>>>>");
         System.out.print("Input nilai UTS: >");
         int nilaiUTS_2 = sc.nextInt();
         System.out.print("Input nilai UAS: >");
@@ -33,17 +34,17 @@ public class CaseMethod_1 {
             nilaiHuruf_1 = "A";
         } else if(nilaiAkhir_1 > 73 && nilaiAkhir_1 <=80){
             nilaiHuruf_1 = "B+";
-        } else if(nilaiAkhir_1 > 73 && nilaiAkhir_1 <=80){
-            nilaiHuruf_1 = "B";
         } else if(nilaiAkhir_1 > 65 && nilaiAkhir_1 <=73){
-            nilaiHuruf_1 = "C+";
+            nilaiHuruf_1 = "B";
         } else if(nilaiAkhir_1 > 60 && nilaiAkhir_1 <=65){
-            nilaiHuruf_1 = "C";
+            nilaiHuruf_1 = "C+";
         } else if(nilaiAkhir_1 > 50 && nilaiAkhir_1 <=60){
-            nilaiHuruf_1 = "D";
+            nilaiHuruf_1 = "C";
         } else if(nilaiAkhir_1 > 39 && nilaiAkhir_1 <=50){
+            nilaiHuruf_1 = "D";
+        }else if(nilaiAkhir_1 <= 39){
             nilaiHuruf_1 = "E";
-        }else {
+        }else{
             nilaiHuruf_1 = "-";
         }
 
@@ -57,17 +58,17 @@ public class CaseMethod_1 {
             nilaiHuruf_2 = "A";
         } else if(nilaiAkhir_2 > 73 && nilaiAkhir_2 <=80){
             nilaiHuruf_2 = "B+";
-        } else if(nilaiAkhir_2 > 73 && nilaiAkhir_2 <=80){
-            nilaiHuruf_2 = "B";
         } else if(nilaiAkhir_2 > 65 && nilaiAkhir_2 <=73){
-            nilaiHuruf_2 = "C+";
+            nilaiHuruf_2 = "B";
         } else if(nilaiAkhir_2 > 60 && nilaiAkhir_2 <=65){
-            nilaiHuruf_2 = "C";
+            nilaiHuruf_2 = "C+";
         } else if(nilaiAkhir_2 > 50 && nilaiAkhir_2 <=60){
-            nilaiHuruf_2 = "D";
+            nilaiHuruf_2 = "C";
         } else if(nilaiAkhir_2 > 39 && nilaiAkhir_2 <=50){
+            nilaiHuruf_2 = "D";
+        }else if(nilaiAkhir_2 <= 39){
             nilaiHuruf_2 = "E";
-        }else {
+        }else{
             nilaiHuruf_2 = "-";
         }
 
@@ -87,24 +88,21 @@ public class CaseMethod_1 {
             statusAkhir = "TIDAK LULUS";
         }
 
-        System.out.println("Nama Mahasiswa: "+namaMhs);
-        System.out.println("NIM "+nim);
-        System.out.println("1. Algoritma Pemrograman");
-        System.out.println("Nilai UTS: "+nilaiUTS_1);
-        System.out.println("Nilai UAS: "+nilaiUAS_1);
-        System.out.println("Nilai Tugas: "+nilaiTugas_1);
-        System.out.println("Nilai Akhir: "+nilaiAkhir_1);
-        System.out.println("Nilai Huruf: "+nilaiHuruf_1);
-        System.out.println("Status "+status_1);
-        System.out.println("2. Struktur Data");
-        System.out.println("Nilai UTS: "+nilaiUTS_2);
-        System.out.println("Nilai UAS: "+nilaiUAS_2);
-        System.out.println("Nilai Tugas: "+nilaiTugas_2);
-        System.out.println("Nilai Akhir: "+nilaiAkhir_2);
-        System.out.println("Nilai Huruf: "+nilaiHuruf_2);
-        System.out.println("Status "+status_2);
-        System.out.println("Rata-rata nilai akhir: "+avg_nilai_akhir);
-        System.out.println("Status Semester: "+statusAkhir);
+        System.out.println("========================================================================");
+        System.out.println("                         HASIL PENILAIAN AKADEMIK                       ");
+        System.out.println("========================================================================");
+        System.out.println("Nama Mahasiswa : " + namaMhs);
+        System.out.println("NIM            : " + nim);
+        System.out.println("========================================================================");
+        System.out.println("| No | Mata Kuliah        | UTS | UAS | Tugas | Akhir | Huruf | Status|");
+        System.out.println("--------------------------------------------------------------------------");
+        System.out.println("| 1  | Algoritma Pemrograman| " + nilaiUTS_1 + "  | " + nilaiUAS_1 + "  | " + nilaiTugas_1 + "   | " + nilaiAkhir_1 + "  | " + nilaiHuruf_1 + "   | " + status_1 + " |");
+        System.out.println("| 2  | Struktur Data        | " + nilaiUTS_2 + "  | " + nilaiUAS_2 + "  | " + nilaiTugas_2 + "   | " + nilaiAkhir_2 + "  | " + nilaiHuruf_2 + "   | " + status_2 + " |");
+        System.out.println("-------------------------------------------------------------------------");
+        System.out.println("Rata-rata Nilai Akhir : " + avg_nilai_akhir);
+        System.out.println("Status Semester       : " + statusAkhir);
+        System.out.println("=========================================================================");
+
 
         sc.close();
     }
